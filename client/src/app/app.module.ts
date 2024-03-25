e0f54d91cb2c85be3f030f6ed2a513c83b6d5da0
mahmoudramzy72 <mahmoudhamdyramzy72@gmail.com>
2024-03-25 18:00:05 +0200

    End Of Section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
-
-
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,12 +9,12 @@ import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { CommonModule } from '@angular/common';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
   ],
   imports: [
     BrowserModule,
@@ -28,7 +26,9 @@ import { HomeModule } from './home/home.module';
     HomeModule
     
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
