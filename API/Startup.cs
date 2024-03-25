d3b06d5d6e60ec3a6edd5bb45f43187612c5f082
mahmoudramzy72 <mahmoudhamdyramzy72@gmail.com>
2024-03-25 04:47:56 +0200

    End Of Section 10

---
+++ API/Startup.cs
@@ -0,0 +1,84 @@
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using AutoMapper;
+using API.Helpers;
+using API.Middleware;
+using API.Extensions;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+       
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.AddAutoMapper(typeof(MappingProfiles));
+
+            services.AddControllers();
+            services.AddDbContext<StoreContext>(x =>
+             x.UseSqlite(_config.GetConnectionString("DefualtConnection")));
+
+
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentaion();
+            services.AddCors(opt => {
+
+                opt.AddPolicy("CordsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
+           
+          
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+            if (env.IsDevelopment())
+            {
+            
+                app.UseSwaggerDocumentation();
+        
+            }
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            // app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    CommonModule,
+    HomeModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
